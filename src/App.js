1c1d150ace309d8bcbb2f05477c2b299194d6673
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2021-12-28 19:26:35 +0200

    integrating bootstrap in the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
+import Login from './components/frontend/auth/Login';
+import Register from './components/frontend/auth/Register';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
           <Route
             path="/admin"
             name="Admin"
---
+++ src/components/frontend/auth/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Register() {
+  return (
+    <div>
+      <h1>Register page</h1>
+      <button className="btn btn-primary">Check</button>
+    </div>
+  );
+}
