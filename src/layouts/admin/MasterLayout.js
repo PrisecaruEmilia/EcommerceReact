c769fbf494dc313af42ca8e4832a25ad943c1a2e
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2021-12-28 17:35:01 +0200

    main layout of admin - FE

---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts';
+import NavBar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+
+const MasterLayout = () => {
+  return (
+    <div className="sb-nav-fixed">
+      <NavBar />
+      <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+          <Sidebar />
+        </div>
+        <div id="layoutSidenav_content">
+          <main>Master File</main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MasterLayout;
