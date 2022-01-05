11d4b0cfcaf730cb621750ecfdfe893f5a8fe375
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-01-05 19:51:46 +0200

    logout done - FE

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import MasterLayout from './layouts/admin/MasterLayout';
 import Home from './components/frontend/Home';
 import Login from './components/frontend/auth/Login';
@@ -11,6 +16,11 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
 
 axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('auth_token');
+  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  return config;
+});
 
 function App() {
   return (
@@ -18,8 +28,23 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/register" component={Register}></Route>
+          {/* <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route> */}
+
+          <Route path="/login">
+            {localStorage.getItem('auth_token') ? (
+              <Redirect to="/" />
+            ) : (
+              <Login></Login>
+            )}
+          </Route>
+          <Route path="/register">
+            {localStorage.getItem('auth_token') ? (
+              <Redirect to="/" />
+            ) : (
+              <Register></Register>
+            )}
+          </Route>
           <Route
             path="/admin"
             name="Admin"
